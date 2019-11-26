d087392ae86d2586eca38947b26b8d1d687979a0
Adish Jain <adishj@opentext.com>
2019-11-26 15:05:00 +0530

    Added image upload feature

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Campground = require("../models/campground");
 const middleware = require("../middleware");
+const multer = require("multer");
+const path = require("path");
+
+// Set storage engine to upload the images
+const storage = multer.diskStorage({
+  destination: "public/uploads/",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+// Init upload to upload the image
+const upload = multer({
+  storage: storage
+}).single("image");
 
 // Index- show all campgrounds
 router.get("/", (req, res) => {
@@ -14,29 +32,36 @@ router.get("/", (req, res) => {
 
 // Create- add new campground to databse
 router.post("/", middleware.isLoggedIn, (req, res) => {
-  // Retrieving new camp data from form.
-  let campName = req.body.name;
-  let imageUrl = req.body.image;
-  let description = req.body.description;
-  let authorId = req.user._id;
-  let authorName = req.user.username;
-  let newCamp = {
-    name: campName,
-    image: imageUrl,
-    description,
-    author: {
-      id: authorId,
-      username: authorName
+  upload(req, res, err => {
+    if (err) {
+      res.render("/campgrounds/new");
+    } else {
+      // Retrieving new camp data from form.
+      let campName = req.body.name;
+      let newUrl = `uploads/${req.file.filename}`;
+      let description = req.body.description;
+      let authorId = req.user._id;
+      let authorName = req.user.username;
+      let newCamp = {
+        name: campName,
+        image: newUrl,
+        description,
+        author: {
+          id: authorId,
+          username: authorName
+        }
+      };
+
+      // Create a new campground and save in db.
+      Campground.create(newCamp)
+        .then(camp => {
+          req.flash("success", "Campground created successfully");
+          res.redirect("/campgrounds");
+        })
+        .catch(err => console.log(err));
+      // Redirecting to campgrounds page
     }
-  };
-  // Create a new campground and save in db.
-  Campground.create(newCamp)
-    .then(camp => {
-      req.flash("success", "Campground created successfully");
-      res.redirect("/campgrounds");
-    })
-    .catch(err => console.log(err));
-  // Redirecting to campgrounds page
+  });
 });
 
 // NEW - show form to create a campground.
